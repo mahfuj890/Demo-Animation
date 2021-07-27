145294dc243f38da185c1a888758f8f5441f3f9a
mahfuj890 <mahfujali490@gmail.com>
2021-07-28 02:54:35 +0600

    add animation horizontal

---
+++ main.js
@@ -0,0 +1,13 @@
+    // Service Card 3D Hover Effect
+    $('.service_items_area').tilt({
+        maxTilt: 15,
+        glare: true,
+        reverse: true,
+        "max-glare": 0.5
+      });
+
+
+      const scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+    });
